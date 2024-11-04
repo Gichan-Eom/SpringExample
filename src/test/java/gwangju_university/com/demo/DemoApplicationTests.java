0acaa74abe5b9d4c06bafbf62f099e9d3ec36deb
313 <313@DESKTOP-I40ONBN>
2024-11-04 16:05:28 +0900

    SpringExample

---
+++ src/test/java/gwangju_university/com/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package gwangju_university.com.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
